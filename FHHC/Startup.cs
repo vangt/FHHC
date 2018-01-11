d6cbac6e46ba4a0e01598e9385cbc190729a3dce
vangt <vangt86@gmail.com>
2018-01-11 13:52:57 -0600

    Add project files.

---
+++ FHHC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FHHC.Startup))]
+namespace FHHC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
