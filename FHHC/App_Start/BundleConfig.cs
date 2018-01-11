e7767795e018590d0a42c5a9f127c66aee1244e5
vangt <vangt86@gmail.com>
2018-01-11 17:36:11 -0600

    Changed bootstrap.

--- FHHC/App_Start/BundleConfig.cs
+++ FHHC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace FHHC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-litera.css",
                       "~/Content/site.css"));
         }
     }
